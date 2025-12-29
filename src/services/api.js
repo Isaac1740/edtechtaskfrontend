2a557b9c0552d2cd6f75a716823480d5fbe313ae
Yata Isaac Pranit Kumar <isaacpranit@gmail.com>
2025-12-29 17:02:57 +0530

    Update API base URL to new endpoint

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://edtech-backend-0q1u.onrender.com",
+  baseURL: "https://edtech-backend-1-f3zs.onrender.com",
 });
 
 export default api;
+
